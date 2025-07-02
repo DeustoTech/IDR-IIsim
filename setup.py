936d9eac6a23177afa9c4bc75b283e6633387a22
x3lab <xabier.cantero@deusto.es>
2025-07-02 08:07:55 +0000

    Execute processes on alphabetical order

--- setup.py
+++ setup.py
@@ -1,46 +1,52 @@
 import importlib
 import os
 
-from idr_iisim.models.model      import Model            # import Model class
-from idr_iisim.utils.config      import GlobalVariables  # read config file and set global variables
-from idr_iisim.utils.execution   import generate_execution_queue
-from idr_iisim.utils.logger      import i_logger
+from idr_iisim.models.model import Model  # import Model class
+from idr_iisim.utils.config import (
+    GlobalVariables,
+)  # read config file and set global variables
+from idr_iisim.utils.execution import generate_execution_queue
+from idr_iisim.utils.logger import i_logger
 from idr_iisim.utils.models_dict import models_dict
-from idr_iisim.utils.schema      import Validator
+from idr_iisim.utils.schema import Validator
 
 # class name of the model
 class_name = "MethodModel"
 
 
 def find_processes() -> None:
-    """ Find all processes in the industry folder and load them into the models_dict """
-    #path = os.path.dirname(os.path.realpath(__file__)) + "/idr_iisim/industry"
-    
-    #get env var INDUSTRY_PATH & set the path
+    """Find all processes in the industry folder and load them into the models_dict"""
+    # path = os.path.dirname(os.path.realpath(__file__)) + "/idr_iisim/industry"
+
+    # get env var INDUSTRY_PATH & set the path
     path = os.environ.get("INDUSTRIES_PATH")
-    
+
     # walk through the industry folder: generating the file names in a directory tree by walking the directory tree either top-down or bottom-up
     # Uses os.walk to traverse the directory tree starting from path.
-    for root, dirs, files in os.walk(path, topdown=False):      # root: The current directory path, dirs: List of subdirectories, files: List of files in the current directory.
+    for root, dirs, files in os.walk(
+        path, topdown=False
+    ):  # root: The current directory path, dirs: List of subdirectories, files: List of files in the current directory.
         # check if the folder contains a model.py file
         if "model.py" in files:
-            # Dynamically loads the model.py file as a Python module: 
+            # Dynamically loads the model.py file as a Python module:
             # loads these modules, creates instances of a specific class (class_name) within each model.py
             model_path = os.path.join(root, "model.py")
-            spec       = importlib.util.spec_from_file_location(class_name, model_path)
-            module     = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(module)                   # execute the module
-            
+            spec = importlib.util.spec_from_file_location(
+                class_name, model_path
+            )
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)  # execute the module
+
             # create an instance of the class
-            instance: Model = getattr(module, class_name)() 
+            instance: Model = getattr(module, class_name)()
 
             # save instance in ModelDict class
             key = instance.config.id
             models_dict.add_model(key=key, model=instance)
-            
+
         # log the key directory of the instance
         i_logger.logger.debug(f"instance: {root}")
-            
+
     # set the loaded flag to True
     models_dict.set_dict_to_loaded()
     # size of the models dictionary: number of models in the dictionary
@@ -54,15 +60,16 @@ def init() -> None:
     # read config file
     i_logger.logger.debug("reading config file")
     GlobalVariables()
-    i_logger.logger.debug("env paths: "   + GlobalVariables().__str__())
+    i_logger.logger.debug("env paths: " + GlobalVariables().__str__())
     i_logger.logger.debug("config file read and global variables set\n")
 
     # init yaml validator
     i_logger.logger.debug("initializing yaml validator")
     Validator()
-    #i_logger.logger.debug("schema: " + str(Validator().validator))  
-    i_logger.logger.debug("yaml validator initialized (based on a schema.yaml)\n")
-
+    # i_logger.logger.debug("schema: " + str(Validator().validator))
+    i_logger.logger.debug(
+        "yaml validator initialized (based on a schema.yaml)\n"
+    )
 
     # find processes
     i_logger.logger.debug("finding processes")
@@ -77,7 +84,7 @@ def main() -> None:
     i_logger.logger.info("iDesignRES tool started")
 
     all_dependencies = models_dict.dependencies
-    for key, value in models_dict.models.items():
+    for key in sorted(models_dict.models):
         queue = generate_execution_queue(key)
 
         # perform calculations for each model in the queue so that the dependencies are met
@@ -89,7 +96,9 @@ def main() -> None:
             if model_instance_id in all_dependencies:
                 # if there are dependencies, then check whether they are already calculated
                 dependencies = all_dependencies[model_instance_id]
-                i_logger.logger.debug(f"checking model dependencies: {dependencies}")
+                i_logger.logger.debug(
+                    f"checking model dependencies: {dependencies}"
+                )
                 for dependency in dependencies:
                     # if calculated, then get the results
                     output_dependency = models_dict.models[dependency].results
@@ -105,16 +114,20 @@ def main() -> None:
             # print diagrams
             instance.print_diagram()
             print("\n\n")
-            
+
             # log input and results
-            i_logger.logger.debug(f"external inputs: {instance.external_inputs}")
+            i_logger.logger.debug(
+                f"external inputs: {instance.external_inputs}"
+            )
             i_logger.logger.debug(f"results: {instance.results}")
-        
+
         # call script_generator
-        instance.script_generator(GlobalVariables().templates_path + GlobalVariables().template_generated_script_filename,
-                                  GlobalVariables().generated_model_script_filename)
-       
-         
+        instance.script_generator(
+            GlobalVariables().templates_path
+            + GlobalVariables().template_generated_script_filename,
+            GlobalVariables().generated_model_script_filename,
+        )
+
 
 print("\n")
 
@@ -123,5 +136,5 @@ pass
 
 if __name__ == "__main__":
     #
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
     main()
