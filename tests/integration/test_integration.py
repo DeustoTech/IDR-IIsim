c1bffe18197fc9037bd3542927263f3162d23c6a
x3lab <xabier.cantero@deusto.es>
2025-10-02 12:56:23 +0000

    Linter fix: test_integration

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -11,69 +11,48 @@ from idr_iisim.utils.models_dict import (  # type:ignore # pylint: disable=impor
 )
 from main import process_industry  # type:ignore # pylint: disable=import-error
 
+INDUSTRIES_PATH = "Sources"
+
 
 class TestIntegration(unittest.TestCase):
     """Loger test class"""
 
     def test_full_integration(self) -> None:
         """Full test"""
-        industries_path = "Sources"
-        for elem in os.listdir(industries_path):
-            elem_path = os.path.join(industries_path, elem)
+        for elem in os.listdir(INDUSTRIES_PATH):
+            elem_path = os.path.join(INDUSTRIES_PATH, elem)
             if os.path.isdir(elem_path):
                 process_industry(elem, elem_path)
                 yaml = _find_industry(elem_path)
-                output_path = os.path.join(
-                    "industries", f"{yaml['short_name'].lower()}.py"
-                )
                 # Check if file is created
-                self.assertTrue(os.path.isfile(output_path))
+                self.assertTrue(
+                    os.path.isfile(
+                        _get_industry_class_path(yaml["short_name"])
+                    )
+                )
                 # Check to import the class
                 try:
-                    module = importlib.import_module(
-                        f"industries.{yaml['short_name'].lower()}"
-                    )
-                    industry_cls = getattr(module, yaml["short_name"], None)
+                    industry_cls = _import_class(yaml["short_name"])
                     self.assertIsNotNone(
                         industry_cls, "The industry has not been created"
                     )
                     tests = yaml["outcome"][0].get("tests", [1])
-                    for i in range(len(tests)):
+                    for i in range(  # pylint: disable=consider-using-enumerate
+                        len(tests)
+                    ):
                         instance = industry_cls(tests[i])
                         self.assertIsNotNone(instance)
-                        # Check demands
-                        for demand in yaml["demands"]:
-                            if "meta" not in demand:
-                                method_name = f"get_{demand['name']}"
-                                values = demand.get("tests", [])
-                                with self.subTest(method=method_name):
-                                    method = getattr(
-                                        instance, method_name, None
-                                    )
-                                    self.assertIsNotNone(method)
-                                    value = method()
-                                    self.assertIsInstance(value, float)
-                                    if i < len(values):
-                                        self.assertEqual(
-                                            values[i], round(value, 2)
-                                        )
-                        # Check meta-demands
-                        for meta_demand in yaml["meta"]:
-                            method_name = f"get_{meta_demand['name']}"
-                            values = demand.get("tests", [])
-                            with self.subTest(method=method_name):
-                                method = getattr(instance, method_name, None)
-                                self.assertIsNotNone(method)
-                                value = method()
-                                self.assertIsInstance(value, float)
-                                if i < len(values):
-                                    self.assertEqual(
-                                        values[i], round(value, 2)
-                                    )
-                        # Check outputs
-                        for output in yaml["outputs"]:
-                            method_name = f"get_{output['name']}"
-                            values = output.get("tests", [])
+                        # Check items (demands, meta-demands and outputs)
+                        items_to_evaluate = [
+                            demand
+                            for demand in yaml["demands"]
+                            if "meta" not in demand
+                        ]
+                        items_to_evaluate.extend(yaml["meta"])
+                        items_to_evaluate.extend(yaml["outputs"])
+                        for item in items_to_evaluate:
+                            method_name = f"get_{item['name']}"
+                            values = item.get("tests", [])
                             with self.subTest(method=method_name):
                                 method = getattr(instance, method_name, None)
                                 self.assertIsNotNone(method)
@@ -93,7 +72,7 @@ class TestIntegration(unittest.TestCase):
                         self.assertIsNone(instance.csv())
                         self.assertIsInstance(instance.csv_header(), list)
                         self.assertIsInstance(instance.csv_row(), list)
-                except Exception:
+                except Exception:  # pylint: disable=broad-exception-caught
                     self.fail("Industry not correctly generated")
 
 
@@ -104,3 +83,14 @@ def _find_industry(industry_path: str) -> dict[str, Any]:
         if yaml_data["type"] == "industry":
             return yaml_data
     raise ValueError("Industry yaml not found!")
+
+
+def _import_class(name: str) -> Any:
+    module = importlib.import_module(f"industries.{name.lower()}")
+    industry_cls = getattr(module, name, None)
+    return industry_cls
+
+
+def _get_industry_class_path(name: str) -> str:
+    output_path = os.path.join("industries", f"{name.lower()}.py")
+    return output_path
