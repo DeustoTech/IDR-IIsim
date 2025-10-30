a95292a996c31f08ec99c39c8bb8e94e11ec99bd
x3lab <xabier.cantero@deusto.es>
2025-10-30 14:48:48 +0000

    Sphinx docs

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../src/"))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "IDR-IIsim"
+copyright = "2025, Xabier Cantero, Alejo Silvarrey, Armando Aguayo, Iván González, Cruz Borges"
+author = "Xabier Cantero, Alejo Silvarrey, Armando Aguayo, Iván González, Cruz Borges"
+release = "0.2.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
